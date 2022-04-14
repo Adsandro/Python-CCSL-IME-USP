ef1ff9ede311875f9a12c562c01a4c2c49affb52
Adsandro <adsandroxerd@gmail.com>
2022-04-14 02:20:40 -0300

    condicionais

---
+++ condicional.py
@@ -0,0 +1,8 @@
+tempoDeJogo = int(input("Quanto tempo temos ja jogados ? "))
+
+if tempoDeJogo <= 90:
+    print("Ainda tem jogo pela frente")
+    print("Que bom!!")
+else:
+    print("Droga, o jogo esta acabando!")
+    print("Apita logo juiz!!")
