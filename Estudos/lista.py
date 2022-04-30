4de696732525bacbd338a5a4cf1e44e5e0b2ce6b
Adsandro <adsandroxerd@gmail.com>
2022-04-30 11:21:37 -0300

    Lista, Arrays - caracteristicas

---
+++ Estudos/lista.py
@@ -0,0 +1,24 @@
+playlist_adsandro = ["wolves", "go", "lost forever  "]
+
+print(playlist_adsandro[0])
+print(playlist_adsandro[-2])
+print(playlist_adsandro[2])
+
+print(len(playlist_adsandro))
+
+filme = ["Star Wars", "George Lucas", "Lucas Filmes", 1977]
+
+
+filme.append("filme muito bom") #adiciona elemento na lista
+
+print(filme[4]) 
+
+filme[3] = 2022 #troca elemento presente na lista
+
+print(filme[3])
+
+print(len(filme))
+
+s = []
+s.append(100)
+print(s)
